99dee7ef5c88cc23fcb79afffb0e73cc757bd67a
jeet-devops <53583887+jeet-devops@users.noreply.github.com>
2022-12-22 10:34:51 -0500

    terraform block updated

--- terraform-block/install.sh
+++ terraform-block/install.sh
@@ -8,4 +8,8 @@ docker --version
 
 sudo usermod -a -G docker "ec2-user"
 
-sudo docker run -d -p 80:80 nginx
+sudo docker run --name nginx -d -p 80:80  nginx
+
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o metadata.html
+
+sudo docker cp metadata.html nginx:/usr/share/nginx/html
