01c768bc68b7bc65028eb52835909ea469162356
jeet-devops <53583887+jeet-devops@users.noreply.github.com>
2022-12-22 11:56:50 -0500

    Terraform ec2 resource created
    * terraform block and resource aws instance
    
    * terraform block updated
    
    * terraform block updated
    
    * terraform block updated
    
    * terraform ec2 instance
    
    * terraform ec2 instance

---
+++ terraform-ec2/main.tf
@@ -0,0 +1,14 @@
+
+resource "aws_instance" "nginx" {
+
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+  user_data     = file("${path.module}/install.sh")
+  tags = {
+    "Name" = "Nginx"
+  }
+
+
+
+
+}
