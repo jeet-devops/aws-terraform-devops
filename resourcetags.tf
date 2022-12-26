3f4780c634c49ec18dcab2bd3e19c8cacd6d4e4d
jeet-devops <53583887+jeet-devops@users.noreply.github.com>
2022-12-26 14:45:02 -0500

    query resource tags output

--- resourcetags.tf
+++ resourcetags.tf
@@ -2,6 +2,6 @@ data "aws_resourcegroupstaggingapi_resources" "test" {}
 
 output "resource_tags" {
 
-    value = data.aws_resourcegroupstaggingapi_resources.test.tags
+    value = data.aws_resourcegroupstaggingapi_resources.test.resource_tag_mapping_list
   
 }
